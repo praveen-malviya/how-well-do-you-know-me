1d47a6258dd80fdc8c1f9d7d5f5314270e91ad9a
praveen-malviya <contact.praveenmalviya@gmail.com>
2021-04-12 07:40:07 +0530

    Feat: CheckAnswer function
    Added the check answer function to validate user answer.

--- index.js
+++ index.js
@@ -3,3 +3,20 @@ var readlineSync = require('readline-sync');
 //To take User Name as Input.
 var userName = readlineSync.question("Please tell us your name..");
 console.log("Hi "+ userName.charAt(0).toUpperCase() + userName.slice(1) +", It's very nice to see you here. ");
+console.log("");
+
+// Adding the FUntion to check Answer
+var score = 0;
+var checkAnswer = function(question, answer){
+
+  var userAnswer = readlineSync.question(question);
+   if (userAnswer.toUpperCase() == answer){
+     console.log("Correct! "+ answer + " is  the Right Answer.");
+     score = score+1;
+   }else
+   {
+      console.log("Wrong! "+ answer + " was the Right Answer.");
+   }  
+   console.log("your score is "+ score +".");
+   console.log("*****************************************")
+  }
